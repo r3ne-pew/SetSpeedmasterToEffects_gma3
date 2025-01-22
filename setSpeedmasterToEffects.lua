133fbb499d3f6ec9663edf2eee74d2fb8eff29cf
Rene Henkenius <mail@r3ne.de>
2025-01-22 13:28:14 +0100

    Update setSpeedmasterToEffects.lua

--- setSpeedmasterToEffects.lua
+++ setSpeedmasterToEffects.lua
@@ -27,8 +27,46 @@ local function gatherUserInput()
     SpeedMaster = TextInput("SpeedMaster")
 end
 
+local function messageBoxUserInput()
+    -- A table with two default buttons for the pop-up
+    local defaultCommandButtons = {
+        { value = 2, name = "OK" },
+        { value = 1, name = "Cancel" }
+    }
+    -- A table with three input fields
+    -- The fields will be displayed alphabetically in the pop-up based on name
+    local inputFields = {
+        { name = "1-PresetPool",  value = "", whiteFilter = "0123456789", vkPlugin = "NumericInput" },
+        { name = "2-PresetFrom",  value = "", whiteFilter = "0123456789", vkPlugin = "NumericInput" },
+        { name = "3-PresetTo",    value = "", whiteFilter = "0123456789", vkPlugin = "NumericInput" },
+        { name = "4-SpeedMaster", value = "" }
+    }
+
+    -- A table with the elements needed for the pop-up
+    local messageTable = {
+        icon = "object_smart",
+        backColor = "Window.Plugins",
+        title = "Set Speedmaster to Effects",
+        message = 'Set the needed values and click "Ok"',
+        commands = defaultCommandButtons,
+        inputs = inputFields,
+        autoCloseOnInput = false
+    }
+
+    -- The creation on the actual pop-up with the result stored in a variable
+    local returnTable = MessageBox(messageTable)
+
+    -- Print the content of the returned table
+    Printf("Success = " .. tostring(returnTable.success))
+
+    SpeedMaster = returnTable.inputs["1-PresetPool"]
+    PresetFrom = returnTable.inputs["2-PresetFrom"]
+    PresetTo = returnTable.inputs["3-PresetTo"]
+    SpeedMaster = returnTable.inputs["4-SpeedMaster"]
+end
+
 function main()
-    gatherUserInput()
+    messageBoxUserInput()
     assign()
 
     print("SpeedMaster " ..
